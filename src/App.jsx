c7d0a8444fd3d25b9c4280359bf8a4bc3a24ba6c
Juan Pablo Pieroni <seisduro@gmail.com>
2023-05-17 19:04:25 -0300

    TailWind

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
+ 
 import "./App.css";
-import Button1 from "./Button1";
+ 
  
 
-const App = () =>  {
-  const [count, setCount] = useState(0);
+const App = () =>  { 
 
   return (
     <>
       <h1>Yaru Cosmetica</h1>
-       <Button1 />
+       
     </>
   
   );
--- src/Button1.jsx
+++
@@ -1,9 +0,0 @@
- 
-
-const Button1 = () => {
-  return (
-   <button>+1</button>
-  )
-}
-
-export default Button1
